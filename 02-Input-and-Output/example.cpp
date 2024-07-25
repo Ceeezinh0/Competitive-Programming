e17f38472af8c551f5697a682465e54684f7a432
cristisiuuu <cristiano.marques@unesp.br>
2024-07-25 10:59:14 -0300

    Add examples for Programming Languages and Input and Output

---
+++ 02-Input-and-Output/example.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    string s = "Test 2";
+
+    cin >> a >> b;
+
+    cout << s << "\n" << "Sum: " << a + b << "\n";
+
+    return 0;
+}
