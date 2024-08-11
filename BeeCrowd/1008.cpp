b7dfba06295d3d41c424a35955fa7bbdaa1e424d
cristisiuuu <cristiano.marques@unesp.br>
2024-08-11 13:10:09 -0300

    Create 1008.cpp

---
+++ BeeCrowd/1008.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int A, B;
+    double C;
+
+    cin >> A >> B >> C;
+
+    cout << "NUMBER = " << A << "\n";
+    cout << fixed << setprecision(2);
+    cout << "SALARY = U$ " << B * C << "\n";
+    
+    return 0;
+}
