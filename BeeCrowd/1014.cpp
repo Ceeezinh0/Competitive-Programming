b92eb017b67095dda0c14c11784571fc5f3b26bc
cristisiuuu <cristiano.marques@unesp.br>
2024-08-12 13:00:53 -0300

    Create 1014.cpp

---
+++ BeeCrowd/1014.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main() {
+    int X;
+    double Y;
+
+    cin >> X >> Y;
+
+    cout << fixed << setprecision(3);
+    cout << X/Y << " km/l" << "\n";
+ 
+    return 0;
+}
