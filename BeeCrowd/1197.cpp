bb2441863276afb9b7c421d9df03ad4cb1340239
cristisiuuu <cristiano.marques@unesp.br>
2024-08-13 18:40:25 -0300

    Create 1197.cpp

---
+++ BeeCrowd/1197.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int V, T, D;
+
+    while (cin >> V >> T) {
+        D = 2 * V * T;
+        
+        cout << D << "\n";
+    }
+
+    return 0;
+}
