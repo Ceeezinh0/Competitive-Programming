5b1ecb26d7440dcfead4fad8d772a259d695821b
cristisiuuu <cristiano.marques@unesp.br>
2024-08-16 09:29:24 -0300

    Finalizado com Sucesso

--- BeeCrowd/1027.cpp
+++ BeeCrowd/1027.cpp
@@ -1,37 +1,52 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+int t[1010];
+int b[1010];
+pair<int,int> coord[1010];
+
 int main() {
-    int N;
+    int n, x, y;
 
-    while (cin >> N && N != 0) {
+    while(cin >> n) {
+        for(int i = 0; i < n; i++) {
+            cin >> x >> y;
 
-        vector<pair<int, int>> points(N);
-        
-        for (int i = 0; i < N; i++) {
-            cin >> points[i].first >> points[i].second;
+            coord[i] = make_pair(x, y);
         }
 
-        sort(points.begin(), points.end());
+        sort(coord, coord + n);
 
-        //PD para encontrar a maior sequência válida
+        t[0] = 1;
+        b[0] = 1;
 
-        vector<int> pd(N, 1);
-        
-        int max_len = 1;
+        for(int i = 1; i < n; i++) {
+            int maxtop = 1, maxbot = 1;
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (points[i].first != points[j].first && abs(points[i].second - points[j].second) == 2) {
-                    pd[i] = max(pd[i], pd[j] + 1);
+            for(int j = 0; j < i; j++) {
+                // Verifica se eu posso colocar o ponto i depois do j em cima
+                if (coord[i].first > coord[j].first && coord[i].second == coord[j].second + 2){
+                    maxtop = max(maxtop, b[j] + 1);
+                }
+                    
+                // Verifica se eu posso colocar o ponto i depois do j em baixo
+                if (coord[i].first > coord[j].first && coord[i].second == coord[j].second - 2){
+                    maxbot = max(maxbot, t[j] + 1);
                 }
             }
-            max_len = max(max_len, pd[i]);
+            t[i] = maxtop;
+            b[i] = maxbot;
         }
-        
-        cout << max_len << "\n";
+        int maximo = 1;
+
+        for(int i = 0; i < n; i++) {
+            maximo = max(maximo, t[i]);
+            maximo = max(maximo, b[i]);
+        }
+
+        cout << maximo << "\n";
     }
-    
+
     return 0;
 }
