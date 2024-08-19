f9b348d2d9c2a4000d0e9bfb8d060f68be906e61
cristisiuuu <cristiano.marques@unesp.br>
2024-08-19 10:34:21 -0300

    Create 1017.cpp

---
+++ BeeCrowd/1017.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int H, V;
+
+    cin >> H >> V;
+
+    cout << fixed << setprecision(3);
+    cout << (H * V) / 12.0 << "\n";
+
+    return 0;
+}
