13ef3df303058dd7d97d989ed6d93c75dd71239a
Cristiano Marques Silva <118139423+Ceeezinh0@users.noreply.github.com>
2024-08-21 09:18:52 -0300

    Update 1317.cpp

--- BeeCrowd/1317.cpp
+++ BeeCrowd/1317.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int states[30];
-int out[30];
+int estado[30];
+int sai[30];
 
 vector<int> v[30];
 vector<string> res[30];
 
 void bfs(int a){
-  states[a] = 1;
-  for(auto it : v[a])
-    if(states[it]==0)
-      bfs(it);
+    estado[a] = 1;
+
+    for(auto it : v[a]){
+            if(estado[it]==0){
+                bfs(it);
+            }
+    }     
 }
 
 int main(){
-  int viz, n;
+    int viz, n;
 
-  while(scanf("%d", &n), n){
-    memset(out, 0, sizeof out);
+    while(scanf("%d", &n), n){
+        memset(sai, 0, sizeof sai);
 
-    for(int i=0; i<=30; i++){
-        v[i].clear();
-	    res[i].clear();
-	}
-
-    for(int i=1; i<=n; i++){
-        while(scanf("%d", &viz), viz){
-            v[i].push_back(viz);
-            out[i]++;
+        for(int i=0; i<=30; i++){
+            v[i].clear();
+            res[i].clear();
         }
-    }
-    int p, t1, t2;
 
+        for(int i=1; i<=n; i++){
+            while(scanf("%d", &viz), viz){
+                v[i].push_back(viz);
+                sai[i]++;
+            }
+        }
+
+    int p, t1, t2;
     char a1[30], a2[30], a3[30];
     
     while(scanf("%d", &p), p){ 
-        memset(states, 0, sizeof states);
+        memset(estado, 0, sizeof estado);
 
 	    scanf("%d %d", &t1, &t2);
 
@@ -48,11 +49,16 @@ int main(){
         scanf("%s %s %s", a1, a2, a3);
 
 		for(int i=1; i<=n; i++){
-			if(states[i]==0) res[i].push_back(a1);
-			else{
-				if(out[i]<t1) res[i].push_back(a1);
-				else if(out[i] < t2) res[i].push_back(a2);
-				else res[i].push_back(a3);
+			if(estado[i]==0){
+                 res[i].push_back(a1);
+            }else{
+				if(sai[i]<t1){
+                    res[i].push_back(a1);
+                }else if(sai[i] < t2){
+                    res[i].push_back(a2);
+                }else{
+                    res[i].push_back(a3);
+                }
 			}
 		}
     }
@@ -65,7 +71,7 @@ int main(){
 		for(auto it : res[i]){
 			cout << it << " ";
 		}
-		cout << endl;
+		cout << "\n";
 	}
   }
   
